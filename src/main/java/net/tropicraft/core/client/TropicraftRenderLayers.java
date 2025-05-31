e84b5558be6f426da1c5e163603b0a6adcd55270
Gegy <gegy1000@gmail.com>
2025-05-31 19:42:34 +0800

    1.21.5 port: Tapir
    Signed-off-by: Ike Johnson-Woods <contact@ike.au>

--- src/main/java/net/tropicraft/core/client/TropicraftRenderLayers.java
+++ src/main/java/net/tropicraft/core/client/TropicraftRenderLayers.java
@@ -102,6 +102,7 @@ public class TropicraftRenderLayers {
     public static ModelLayerLocation HUMMINGBIRD_LAYER;
     public static ModelLayerLocation JAGUAR_LAYER;
     public static ModelLayerLocation TAPIR_LAYER;
+    public static ModelLayerLocation TAPIR_BABY_LAYER;
     public static ModelLayerLocation SPIDER_MONKEY_LAYER;
     public static ModelLayerLocation WHITE_LIPPED_PECCARY_LAYER;
     public static ModelLayerLocation GIBNUT_LAYER;
@@ -167,6 +168,7 @@ public class TropicraftRenderLayers {
         HUMMINGBIRD_LAYER = registerMain("hummingbird", HummingbirdModel::create, event);
         JAGUAR_LAYER = registerMain("jaguar", JaguarModel::create, event);
         TAPIR_LAYER = registerMain("tapir", TapirModel::create, event);
+        TAPIR_BABY_LAYER = registerMain("tapir_baby", TapirModel::createBaby, event);
         SPIDER_MONKEY_LAYER = registerMain("spider_monkey", SpiderMonkeyModel::create, event);
         WHITE_LIPPED_PECCARY_LAYER = registerMain("white_lipped_peccary", WhiteLippedPeccaryModel::create, event);
         GIBNUT_LAYER = registerMain("gibnut", GibnutModel::create, event);
